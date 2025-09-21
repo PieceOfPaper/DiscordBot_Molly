99c9b2ccfa6236ebb467e6b2d4129871d3432ab8
이성희 <the.paper@live.co.kr>
2025-09-21 23:09:05 +0900

    홀리몰리 명령어 추가

--- Commands/SimpleCommand.cs
+++ Commands/SimpleCommand.cs
@@ -1,3 +1,4 @@
+using Discord;
 using Discord.Interactions;
 
 namespace DiscordBot_Molly.Commands;
@@ -9,4 +10,28 @@ public class SimpleCommand :  InteractionModuleBase<SocketInteractionContext>
 
     [SlashCommand("시간", "현재 시간을 출력합니다.")]
     public async Task Command_Time() => await RespondAsync($"현재 시간은 **{MobiTime.now:yyyy-MM-dd HH:mm:ss}** 입니다 🕒");
+
+    [SlashCommand("홀리몰리", "충격적이다.")]
+    public async Task Command_HollyMolly() => await ProcessImageCommand("hollymolly.png");
+
+
+    private async Task ProcessImageCommand(string fileName)
+    {
+        await DeferAsync();
+
+        var path = Path.Combine(AppContext.BaseDirectory, "assets", fileName);
+        if (!File.Exists(path))
+        {
+            await FollowupAsync("이미지 파일을 찾을 수 없어요.");
+            return;
+        }
+
+        await using var fs = File.OpenRead(path);
+        var embed = new EmbedBuilder()
+            .WithImageUrl($"attachment://{fileName}")
+            .WithColor(Color.Blue)
+            .Build();
+        
+        await FollowupWithFileAsync(fs, fileName, embed: embed);
+    }
 }
